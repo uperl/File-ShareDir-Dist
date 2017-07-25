26d9ae7b9383e0e9317a7b26fe270d7ab133173f
Graham Ollis <plicease@cpan.org>
2017-07-25 14:09:44 -0400

    Initial commit

---
+++ lib/File/ShareDir/Dist.pm
@@ -0,0 +1,10 @@
+package File::ShareDir::Dist;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Locate perl-dist shared files
+# VERSION
+
+1;
---
+++ t/file_sharedir_dist.t
@@ -0,0 +1,6 @@
+use Test2::V0;
+use File::ShareDir::Dist;
+
+ok 1, 'todo';
+
+done_testing
